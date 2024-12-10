b21b2c89a22320b177fd379c7ec891b4b40766bb
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-11 01:31:03 +0600

    Routes Setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from './routes/Router';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ src/routes/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import NotFound from "../pages/NotFound/NotFound";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+    ]
+  },
+  {
+    path: "*",
+    element: <NotFound></NotFound>,
+  },
+]);
+
+export default router;
