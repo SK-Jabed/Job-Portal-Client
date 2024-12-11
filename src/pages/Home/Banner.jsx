d85193cfeea7caf970a45028a86a8de5c17ed38e
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-11 22:59:48 +0600

    Logout User and Banner Animation Done

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import { motion } from "framer-motion";
+import { easeOut, motion } from "framer-motion";
+import bannerOne from "../../assets/banner1.png"
+import bannerTwo from "../../assets/banner2.png"
+
 
 const Banner = () => {
     return (
-      <div className="hero bg-base-200 min-h-[580px]">
+      <div className="hero bg-[#F2F6FD] min-h-[580px]">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+            <div>
+
+          <motion.img
+            animate={{y: [50, 100, 50]}}
+            transition={{duration: 4, repeat: Infinity}}
+            src={bannerOne}
+            className=""
           />
-          <div className='w-3/4'>
-            <motion.h1 className="text-5xl font-bold">Latest Jobs for You</motion.h1>
+          <motion.img
+            animate={{x: [50, 100, 50]}}
+            transition={{duration: 4, delay: 2, repeat: Infinity}}
+            src={bannerTwo}
+            className=""
+          />
+            </div>
+
+          <div className="w-2/4">
+            <motion.h1
+              animate={{ x: 50 }}
+              transition={{
+                duration: 2,
+                delay: 1,
+                ease: easeOut,
+                repeat: Infinity,
+              }}
+              className="text-5xl font-bold"
+            >
+              Latest <motion.span animate={{color: ["#ecff33", "#33ffe3", "#ff6133"]}} transition={{duration: 1.5, repeat: Infinity}}>Jobs</motion.span> for You
+            </motion.h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
