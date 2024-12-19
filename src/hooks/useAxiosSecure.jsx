cd04e82b0149952aa3c6690dd87bfe2c9999b7f5
Sheikh Jabed <jabedbd2295@gmail.com>
2024-12-19 23:52:51 +0600

    Logout and Navigate User After Forbidden or Unauthorized User Done

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000",
@@ -7,11 +9,25 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+    const { logoutUser } = useAuth();
+    const navigate = useNavigate();
+
     useEffect(() => {
         axiosInstance.interceptors.response.use(response => {
             return response;
         }, error => {
             console.log("Error Caught in the interceptor", error);
+
+            if (error.status === 401 || error.status === 403) {
+                console.log("Need to log out the user");
+                logoutUser()
+                .then(() => {
+                    console.log("Logged out user");
+                    navigate("/auth/login");
+                })
+                .catch(error => console.log(error))
+            }
+
             return Promise.reject(error);
         })
     }, [])
