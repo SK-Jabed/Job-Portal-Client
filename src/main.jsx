331eb3934281659accb52e8a30f17d50a261307b
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-17 23:46:16 +0600

    Job Portal Project Client's Main JSX File Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from './routes/Router';
-import AuthProvider from './context/AuthContext/AuthProvider';
+import router from "./routes/Router";
+import AuthProvider from "./context/AuthContext/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
